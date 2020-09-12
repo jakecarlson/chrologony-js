1258ad36653b8b490769f3975f26b6cd52fde029
Jake Carlson <jake@carlsonville.com>
2020-09-12 16:02:41 -0500

    Added welcome email and refactored email sending in general.

--- imports/ui/content/privacy.js
+++ imports/ui/content/privacy.js
@@ -1,10 +1,19 @@
 import './privacy.html';
+import {Meteor} from "meteor/meteor";
 
 Template.privacy.onCreated(function privacyOnCreated() {
 
 });
 
 Template.privacy.helpers({
+
+    appName() {
+        return Meteor.settings.public.app.name;
+    },
+
+    appUrl() {
+        return Meteor.absoluteUrl();
+    },
 
 });
 
