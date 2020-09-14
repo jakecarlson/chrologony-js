97ec69d31079e0bfcd2684e7c8596151bf7486e4
Jake Carlson <jake@carlsonville.com>
2020-09-13 23:46:28 -0500

    Fixed tour direct link.

--- imports/ui/lobby/lobby.js
+++ imports/ui/lobby/lobby.js
@@ -8,9 +8,18 @@ import './categories_manager.js';
 import './clues_manager.js';
 
 Template.lobby.onCreated(function lobbyOnCreated() {
+
     this.autorun(() => {
+
+        FlowRouter.watchPathChange();
+        if (FlowRouter.current().context.hash == 'tour') {
+            TourGuide.start();
+        }
+
         LoadingState.stop();
+
     });
+
 });
 
 Template.lobby.helpers({
