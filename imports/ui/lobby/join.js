e32b493e99f52a1ac5e521279b54f825cdf9d2a2
Jake Carlson <jake@carlsonville.com>
2020-10-17 14:17:45 -0500

    Added errors for leaving room inputs blank.

--- imports/ui/lobby/join.js
+++ imports/ui/lobby/join.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { FlowRouter  } from 'meteor/ostrio:flow-router-extra';
+import { Session } from 'meteor/session';
 import { Flasher } from '../flasher';
 import { LoadingState } from '../../modules/LoadingState';
 
@@ -28,6 +29,19 @@ Template.join.events({
         const name = target.name.value;
         const password = target.password.value;
 
+        // Short circuit if no room name was supplied
+        if (name.trim().length == 0) {
+            Flasher.set('danger', "You must enter a room name.");
+            return;
+        }
+
+        // Short circuit if no password was supplied
+        if (password.trim().length == 0) {
+            Flasher.set('danger', "You must enter a room password.");
+            return;
+        }
+
+        // If this was a create action, validate the room name is unique
         if (i.action == 'create') {
             Meteor.call('room.create', name, password, function(err, id) {
                 if (err) {
@@ -40,6 +54,8 @@ Template.join.events({
                     TourGuide.resume();
                 }
             });
+
+        // Otherwise if this is a join action, validate that the name and password match an existing room
         } else {
             Meteor.call('room.join', name, password, function(err, id) {
                 if (err) {
