c9a80b58f1f52006aae5d6d2441a0de7a0760477
Jake Carlson <jake@carlsonville.com>
2020-12-18 10:22:30 -0600

    Version bump.

--- mobile-config.js
+++ mobile-config.js
@@ -6,7 +6,7 @@ App.info({
     author: 'Jake Carlson',
     email: 'chrologony@carlsonville.com',
     website: 'https://chrologony.com',
-    version: '2020.12.14'
+    version: '2020.12.18'
 });
 
 // Set general Cordova preferences
